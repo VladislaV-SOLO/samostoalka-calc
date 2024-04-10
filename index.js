dfe1b9f9756d3487c2b3dfa9e3b2627696f4b3d7
Vlad Solomahin <vlad.good47@mail.ru>
2024-04-11 00:01:12 +0300

    the end

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const sign = document.querySelector('.container-2-content-sign-select');
-const result = document.querySelector('.result')
-const num1 = document.querySelector('.container-1-content-input')
-const num2 = document.querySelector('.container-3-content-input')
-console.log(num1);
-// const resultTitle = 
+const signSelect = document.querySelector('.container-2-content-sign-select');
+const result = document.querySelector('.result-btn');
+const firstNumInput = document.querySelector('.container-1-content-input');
+const secondNumInput = document.querySelector('.container-3-content-input');
+const resultTitle = document.querySelector('.result-value');
+const reset = document.querySelector('.reset-btn');
 
 
 const operation = {
@@ -11,63 +11,78 @@ const operation = {
     subtract: '-',
     multiply: '*',
     divide: '/',
-}
+};
 
 const calculateData = {
     num1: '',
     num2: '',
-    sign: '',
+    sign: '+',
     result: '',
-}
+};
 
-function sum(num1, num2) {
-    return (+num1) + (+num2)
-}
-function subtract(num1, num2) {
-    return (+num1) - (+num2)
-}
-function multiply(num1, num2) {
-    return (+num1) * (+num2)
-}
-function divide(num1, num2) {
-    return (+num1) / (+num2)
-}
 
-function calculate(num1, num2, sign) {
+
+function calculate({num1, num2, sign}) {
     switch (sign) {
         case operation.sum:
-            calculate.result = String(sum(num1, num2))
+            calculateData.result = sum(num1, num2);
             break;
         case operation.subtract:
-            calculate.result = String(subtract(num1, num2))
+            calculateData.result = subtract(num1, num2);
             break;
         case operation.multiply:
-            calculate.result = String(multiply(num1, num2))
+            calculateData.result = multiply(num1, num2);
             break;
         case operation.divide:
-            calculate.result = String(divide(num1, num2))
+            calculateData.result = divide(num1, num2);
             break;
     }
 }
 
+function sum(num1, num2) {
+    return (+num1) + (+num2);
+}
+function subtract(num1, num2) {
+    return (+num1) - (+num2);
+}
+function multiply(num1, num2) {
+    return (+num1) * (+num2);
+}
+function divide(num1, num2) {
+    return (+num1) / (+num2);
+}
 
 
-num1.addEventListener('keyup', function (event) {
-    calculateData.num1 = event.target.value
-    console.log(calculateData.num1);
-})
-num2.addEventListener('keyup', function (event) {
-    calculateData.num2 = event.target.value
-    console.log(calculateData.num2);
-})
+function resetCalculate() {
+    calculateData.num2 = '';
+    calculateData.num1 = '';
+    calculateData.sign = '+';
+    calculateData.result = '';
+    resultTitle.textContent = '...';
+    firstNumInput.value = '';
+    secondNumInput.value = '';
+    signSelect.value = '+';
+  }
 
-sign.addEventListener('change', function (event) {
-    calculateData.sign = event.target.value
-    console.log(event.target.value);
-})
 
-btn1.addEventListener('click', function(event) {
+  firstNumInput.addEventListener('keyup', function (e) {
+    calculateData.num1 = e.target.value;
+    // console.log(calculateData.num1);
+});
+secondNumInput.addEventListener('keyup', function (e) {
+    calculateData.num2 = e.target.value;
+    // console.log(calculateData.num2);
+});
+
+signSelect.addEventListener('change', function (e) {
+    calculateData.sign = e.target.value;
+    // console.log(event.target.value);
+});
+
+result.addEventListener('click', function(e) {
     calculate(calculateData);
-    result.textContent = calculateData.result
-    
-    })
+    resultTitle.textContent = calculateData.result;
+    console.log(calculateData.result);
+});
+
+reset.addEventListener('click', resetCalculate);
